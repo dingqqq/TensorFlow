b2d0946012f443bdab700353cb593dfba3b8bb2b
Quan Ding <dingquanzjl@hotmail.com>
2017-05-25 00:01:24 -0700

    Added wide and deep learning on census income

--- wideAndDeepLearningCensusIncome.py
+++ wideAndDeepLearningCensusIncome.py
@@ -1,4 +1,53 @@
+import tempfile
+import pandas as pd
 import tensorflow as tf
+
+train_file = "adult.data"
+test_file = "adult.test"
+
+COLUMNS = ["age", "workclass", "fnlwgt", "education", "education_num",
+           "marital_status", "occupation", "relationship", "race", "gender",
+           "capital_gain", "capital_loss", "hours_per_week", "native_country",
+           "income_bracket"]
+
+df_train = pd.read_csv(train_file, names=COLUMNS, skipinitialspace=True)
+df_test = pd.read_csv(test_file, names=COLUMNS, skipinitialspace=True, skiprows=1)
+
+LABEL_COLUMN = "label"
+df_train[LABEL_COLUMN] = (df_train["income_bracket"].apply(lambda x: ">50K" in x)).astype(int)
+df_test[LABEL_COLUMN] = (df_test["income_bracket"].apply(lambda x: ">50K" in x)).astype(int)
+
+CATEGORICAL_COLUMNS = ["workclass", "education", "marital_status", "occupation",
+                       "relationship", "race", "gender", "native_country"]
+CONTINUOUS_COLUMNS = ["age", "education_num", "capital_gain", "capital_loss", "hours_per_week"]
+
+
+# Converting Data into Tensors
+def input_fn(df):
+    # Creates a dictionary mapping from each continuous feature column name (k) to
+    # the values of that column stored in a constant Tensor.
+    continuous_cols = {k: tf.constant(df[k].values) for k in CONTINUOUS_COLUMNS}
+
+    # Creates a dictionary mapping from each categorical feature column name (k)
+    # to the values of that column stored in a tf.SparseTensor.
+    categorical_cols = {k: tf.SparseTensor(indices=[[i, 0] for i in range(df[k].size)], values=df[k].values, dense_shape=[df[k].size, 1]) for k in CATEGORICAL_COLUMNS}
+
+    # Merges the two dictionaries into one.
+    feature_cols = dict(continuous_cols.items() + categorical_cols.items())
+
+    # Converts the label column into a constant Tensor.
+    label = tf.constant(df[LABEL_COLUMN].values)
+
+    # Returns the feature columns and the label.
+    return feature_cols, label
+
+
+def train_input_fn():
+    return input_fn(df_train)
+
+
+def eval_input_fn():
+    return input_fn(df_test)
 
 # Categorical base columns.
 gender = tf.contrib.layers.sparse_column_with_keys(column_name="gender", keys=["Female", "Male"])
@@ -35,3 +84,16 @@ deep_columns = [
     tf.contrib.layers.embedding_column(occupation, dimension=8),
     age, education_num, capital_gain, capital_loss, hours_per_week
 ]
+
+# Combining Wide and Deep Models into One
+model_dir = tempfile.mkdtemp()
+m = tf.contrib.learn.DNNLinearCombinedClassifier(model_dir=model_dir,
+                                                 linear_feature_columns=wide_columns,
+                                                 dnn_feature_columns=deep_columns,
+                                                 dnn_hidden_units=[100, 50])
+
+# Training and Evaluating The Model
+m.fit(input_fn=train_input_fn, steps=200)
+results = m.evaluate(input_fn=eval_input_fn, steps=1)
+for key in sorted(results):
+    print("%s: %s" % (key, results[key]))
